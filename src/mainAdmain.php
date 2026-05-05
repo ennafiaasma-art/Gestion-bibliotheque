1ed814d9be113321a7d40e6487a84d7efd8345ce
ennafiaasma-art <ennafiaasma@gmail.com>
2026-05-05 17:30:15 +0100

    la creation de la partie affichage mainAdmain

---
+++ src/mainAdmain.php
@@ -0,0 +1,35 @@
+<?php
+
+$choice = readline("
+===== MENU =====
+1. Ajouter livre  (titre , auteur,isbn)
+2. Afficher livres avec etat (disponible,emprunte,perdu)
+3.Supprimer livre
+4. creer un compte membre (nom, email) type (etudient,professeur)
+
+
+Choix : ");
+$choice = trim(fgets(STDIN));
+
+switch ($choice) {
+
+    case 1:
+     
+        break;
+
+    case 2:
+      
+        break;
+
+    case 3:
+     
+        break;
+
+    case 4:
+    
+        break;
+
+
+     
+}
+?>
