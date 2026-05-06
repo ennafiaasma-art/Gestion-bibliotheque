85f8188d91448b48672ec585afc26ebdfd80712d
ennafiaasma-art <ennafiaasma@gmail.com>
2026-05-06 12:51:32 +0100

    la connectivite avec data base

--- src/services/library.php
+++ src/services/library.php
@@ -1,6 +1,31 @@
 <?php
+require_once "Db.php";
 
-echo "bonjour";
+class Library {
+    private $db;
 
+    public function __construct() {
+        $database = new Db();
+        $this->db = $database->connect();
+    }
 
+    // 🔹 ajouter un livre
+    public function addBook($title, $author, $year) {
+        try {
+            $sql = "INSERT INTO books (title, author, year) VALUES (:title, :author, :year)";
+            
+            $stmt = $this->db->prepare($sql);
+
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':author', $author);
+            $stmt->bindParam(':year', $year);
+
+            $stmt->execute();
+
+            return "Livre ajouté avec succès";
+        } catch(PDOException $e) {
+            return "Erreur: " . $e->getMessage();
+        }
+    }
+}
 ?>
