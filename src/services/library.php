305044c2c171636a67423c4de840884e5826ac7d
jihanejador <jihanejador@gmail.com>
2026-05-06 11:59:34 +0100

    creation le class avec le constructeur

---
+++ src/services/library.php
@@ -0,0 +1,13 @@
+<?php
+namespace LibCore\Services;
+
+use PDO;
+use PDOException;
+
+class Library {
+    private $db;
+
+    public function _ _construct($dbConnection){
+        $this->db = $dbConnection;
+    }
+}
