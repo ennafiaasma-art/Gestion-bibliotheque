c93489d218eadf48f3a664d4bfd2f14a1437d480
jihanejador <jihanejador@gmail.com>
2026-05-07 16:48:52 +0100

    connecter le data base avec le projet

--- src/mainMember.php
+++ src/mainMember.php
@@ -1,3 +1,9 @@
 <?php
 require_once 'src/services/library.php';
 
+try{
+    $pdo = new PDO("mysql:host=loclhost;dbname=libcore","root","");
+
+}catch(PDOException $e){
+    die("Error: ". $e->getMessage());
+}
